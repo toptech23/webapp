70adabde1cf2ee97a6a63d5da83a71619eef40aa
John <topmangeo22@gmail.com>
2023-12-27 05:33:05 +0100

    My first commit 21

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+    <title>App Deployment</title>
+</head>
+<body>
+<hi>My Web App Deployment</h1>
+<p>Lorem ipsum dolar sit amet, consectetur adipisicing elit.Qwo quisquam assumenda cosequuntur illum solution</i>
+<i>I Love You</i>
+</body>
+</html>
